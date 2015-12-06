e981410c6e86b750788d3622746aad1f4b007c81
Daniel Mortensen <daniel.j.mortensen@gmail.com>
2015-12-06 21:16:04 +0000

    updated manifest, added support for eu domains

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 var elem = document.createElement('img');
-console.log(window.location.host);
-//console.log(window.location.href);
 var host = window.location.host.substring(0,2);
-console.log(host);
-if (host == 'na'){
+
+if (host == 'na' || host == 'eu'){
 	var imgUrl = "chrome-extension://" + chrome.runtime.id + "/resources/production.png"
-	console.log("img url = " + imgUrl);
 	elem.setAttribute('src', imgUrl);
 } else {
 	var imgUrl = "chrome-extension://" + chrome.runtime.id + "/resources/sandbox.png"
-	console.log("img url = " + imgUrl);
 	elem.setAttribute('src', imgUrl);
 }
 
